9332e0f007f65523317500bce654a3034ef07601
Rakshan200417 <kukarakshan2004@gmail.com>
2026-06-14 12:48:05 +0530

    Update adminClient baseURL to use environment variable

--- FindMyMeds-master/FindMyMeds-master/frontend/src/API/adminClient.js
+++ FindMyMeds-master/FindMyMeds-master/frontend/src/API/adminClient.js
@@ -1,17 +1,12 @@
 import axios from "axios";
 
-// IMPORTANT:
-// If you use Vite proxy, set baseURL to "" and use "/api" URLs.
-// Otherwise set baseURL to "http://localhost:8081".
 const adminClient = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8080",
   timeout: 20000,
 });
 
-// Add a request interceptor to attach the JWT token
 adminClient.interceptors.request.use(
   (config) => {
-    // Try to get token from various possible storage keys
     const token =
       localStorage.getItem("token") ||
       localStorage.getItem("pharmacyToken") ||
