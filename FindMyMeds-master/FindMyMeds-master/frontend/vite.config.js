f5c4bb5bdbd95515fe43463002ae87fe161ef910
Rakshan200417 <kukarakshan2004@gmail.com>
2026-06-16 15:06:23 +0530

    Remove path alias configuration in vite.config.js
    Removed path alias configuration from Vite config.

--- FindMyMeds-master/FindMyMeds-master/frontend/vite.config.js
+++ FindMyMeds-master/FindMyMeds-master/frontend/vite.config.js
@@ -1,17 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     include: ['jspdf', 'jspdf-autotable'],
-  },
-  resolve: {
-    alias: [
-      { find: /^(\.\.\/)+[Aa][Pp][Ii]\//, replacement: path.resolve(__dirname, 'src/API') + '/' },
-      { find: /^(\.\.\/)+[Ss]ervice\//, replacement: path.resolve(__dirname, 'src/services') + '/' },
-      { find: /^(\.\.\/)+[Ss]ervices\//, replacement: path.resolve(__dirname, 'src/services') + '/' },
-    ],
   },
 })
